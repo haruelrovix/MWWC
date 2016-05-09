168a5f8513972061e285dc0acaea6c7c05490a9d
haruelrovix <HavitChoirul.Rovik@mitrais.com>
2016-05-09 10:19:15 +0700

    Add Newtonsoft.Json

---
+++ Services.WPF/Employee.cs
@@ -0,0 +1,35 @@
+﻿namespace Services.WPF
+{
+    public class Employee
+    {
+        public string EmployeeID
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string JoiningDate
+        {
+            get;
+            set;
+        }
+
+        public string CompanyName
+        {
+            get;
+            set;
+        }
+
+        public string Address
+        {
+            get;
+            set;
+        }
+    }
+}
--- Services.WPF/MainWindow.xaml.cs
+++ Services.WPF/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using Services.Cpp.CLI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using Services.Cpp.CLI;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Services.WPF
 {
@@ -37,7 +25,7 @@ namespace Services.WPF
 
         private void addEmployee_Click(object sender, RoutedEventArgs e)
         {
-            String str = "{ ";
+            string str = "{ ";
             str += "\"EmployeeID\": \"" + employeeId.Text;
             str += "\", \"Name\": \"" + name.Text;
             str += "\", \"JoiningDate\": \"" + joiningDate.Text;
@@ -55,7 +43,14 @@ namespace Services.WPF
         {
             using (Service wrapper = new Service())
             {
-                MessageBox.Show(wrapper.GetById(employeeId.Text));
+                string json = wrapper.GetById(employeeId.Text);
+
+                Employee employee = JsonConvert.DeserializeObject<Employee>(json);
+
+                name.Text = employee.Name;
+                joiningDate.Text = employee.JoiningDate;
+                companyName.Text = employee.CompanyName;
+                address.Text = employee.Address;
             }
         }
 
